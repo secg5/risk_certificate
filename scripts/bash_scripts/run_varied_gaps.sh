0d57fd24c4f69b69933e98d28e9854d30f31d73e
Naveen Janaki Raman <naveenr@feif-i7.isri.cmu.edu>
2024-09-26 21:46:30 -0400

    Updated plots

--- scripts/bash_scripts/run_varied_gaps.sh
+++ scripts/bash_scripts/run_varied_gaps.sh
@@ -29,26 +29,26 @@ do
 
         uniform_low=0.5
         uniform_high=1
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
 
         uniform_low=0.75
         uniform_high=1
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
 
         uniform_low=0.9
         uniform_high=1
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
 
         uniform_low=0
         uniform_high=0.1
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
 
         uniform_low=0
         uniform_high=0.25
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
 
         uniform_low=0
         uniform_high=0.5
-        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --uniform_low ${uniform_low} --uniform_high ${uniform_high} --run_all_k" ENTER
     done 
 done 
