dea538ab1fa9ef9ba8e37cb1248a14930f124605
Naveen Janaki Raman <naveenr@feif-i7.isri.cmu.edu>
2024-09-27 22:59:50 -0400

    Updated Plots

--- scripts/bash_scripts/run_baseline.sh
+++ scripts/bash_scripts/run_baseline.sh
@@ -20,37 +20,37 @@ do
         delta=0.1
         max_pulls_per_arm=50
 
-        for first_stage_pulls_per_arm in 5 10 15 20 25 30 35 40 45 
+        for first_stage_pulls_per_arm in 25 # 5 10 15 20 25 30 35 40 45 
         do 
-            for n_arms in 5 10
-            do
-                tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
-            done 
+            # for n_arms in 5 10
+            # do
+            #     tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+            # done 
 
             for n_arms in 20 50
             do
-                tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm}" ENTER
-            done 
-        done 
-
-        n_arms=10
-        for max_pulls_per_arm in 10 20 30 50 100 200 400
-        do 
-            frac=0.5 
-            first_stage_pulls_per_arm=$(echo "${max_pulls_per_arm}*${frac}" | bc)
-            first_stage_pulls_per_arm=$(printf "%.0f" $first_stage_pulls_per_arm)
-            tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
-        done 
-
-        for max_pulls_per_arm in 10 20 50 100 200 400
-        do 
-            for frac in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-            do 
-                first_stage_pulls_per_arm=$(echo "${max_pulls_per_arm}*${frac}" | bc)
-                first_stage_pulls_per_arm=$(printf "%.0f" $first_stage_pulls_per_arm)
                 tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
             done 
         done 
+
+        # n_arms=10
+        # for max_pulls_per_arm in 10 20 30 50 100 200 400
+        # do 
+        #     frac=0.5 
+        #     first_stage_pulls_per_arm=$(echo "${max_pulls_per_arm}*${frac}" | bc)
+        #     first_stage_pulls_per_arm=$(printf "%.0f" $first_stage_pulls_per_arm)
+        #     tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        # done 
+
+        # for max_pulls_per_arm in 10 20 50 100 200 400
+        # do 
+        #     for frac in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+        #     do 
+        #         first_stage_pulls_per_arm=$(echo "${max_pulls_per_arm}*${frac}" | bc)
+        #         first_stage_pulls_per_arm=$(printf "%.0f" $first_stage_pulls_per_arm)
+        #         tmux send-keys -t match_${session} "conda activate ${conda_env}; python all_policies.py --seed ${seed} --out_folder ${out_folder} --trials ${trials} --arm_distribution ${arm_distribution} --delta ${delta} --n_arms ${n_arms} --max_pulls_per_arm ${max_pulls_per_arm} --first_stage_pulls_per_arm ${first_stage_pulls_per_arm} --run_all_k" ENTER
+        #     done 
+        # done 
 
     done 
 done 
